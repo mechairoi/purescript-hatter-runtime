4a8e5df103efd4b83a7871e61dc1bd8a029647a2
mechairoi <ttsujikawa@gmail.com>
2017-02-22 08:39:11 +0900

    for purescript 0.10

--- src/Text/Hatter/Runtime.purs
+++ src/Text/Hatter/Runtime.purs
@@ -1,9 +1,16 @@
-module Text.Hatter.Runtime
-       ( Coerce
-       , coerce ) where
+module Text.Hatter.Runtime (class Coerce, coerce) where
+
+import VirtualDOM.VTree as V
+import Prelude
 
 class Coerce a b where
   coerce :: a -> b
 
 instance idCoerce :: Coerce a a where
   coerce = id
+
+instance stringNodeCoerce :: Coerce String V.VTree where
+  coerce s = V.vtext s
+
+instance nodesCoerce :: Coerce V.VTree (Array V.VTree) where
+  coerce a = [a]
